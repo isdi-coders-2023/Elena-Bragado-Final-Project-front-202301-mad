4d221c8caa1c9ba9eeaa133414551a1464256d02
Elena <ebromeo88@gmail.com>
2023-03-17 15:37:53 +0100

    Modify login test

--- src/components/login/login.test.tsx
+++ src/components/login/login.test.tsx
@@ -5,7 +5,7 @@ import { store } from "../../store/store";
 import Login from "./login";
 import { MemoryRouter } from "react-router";
 
-describe("Given the Login component", () => {
+describe("Given the Login components", () => {
   beforeEach(async () => {
     render(
       <Provider store={store}>
