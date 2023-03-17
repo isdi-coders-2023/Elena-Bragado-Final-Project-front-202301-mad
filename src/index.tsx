9caac9e5ae8e41129adb68e1cd5067a1e0cd0082
Elena <ebromeo88@gmail.com>
2023-03-17 10:08:46 +0100

    add index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/app/app";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+
+import Provider from "react-redux/es/components/Provider";
+import { MemoryRouter as Router } from "react-router-dom";
+import { store } from "./store/store";
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
