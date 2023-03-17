17ef1659a30c8953125dd53722077004531cb765
Elena <ebromeo88@gmail.com>
2023-03-17 10:29:37 +0100

    Modify app router

--- src/components/app.router/app.router.tsx
+++ src/components/app.router/app.router.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from "react";
 import { Routes } from "react-router";
-import { Route } from "react-router-dom";
 
 export function AppRouter() {
   return (
     <Suspense>
-      <Routes>
-        <Route path={"/"}></Route>
-      </Routes>
+      <Routes></Routes>
     </Suspense>
   );
 }
