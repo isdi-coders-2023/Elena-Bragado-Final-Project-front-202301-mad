a6a9349b30391f82b908797e414ce5d90a37a5c1
Elena <ebromeo88@gmail.com>
2023-05-03 10:01:38 +0200

    Modify header inputs

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,22 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import styles from "./header.module.scss";
 
 export function Header() {
+  // const lastName = useSelector(
+  //   (state: RootState) => state.users.userLogged.lastName
+  // );
+
   return (
     <header className={styles.header}>
-      <img src="../../../logo.png" alt="logo" />
-      <h1>HomeClick!</h1>
-      <p>Facility servicies</p>
+      <div className={styles.header_container}>
+        <img src="../../../logo.png" alt="logo" />
+        <div className={styles.title_container}>
+          <h1>HomeClick!</h1>
+          <p>Facility servicies</p>
+        </div>
+        {/* <Navbar /> */}
+      </div>
     </header>
   );
 }
