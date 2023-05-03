801fea77591951e870861245e8d260e1446b0729
Elena <ebromeo88@gmail.com>
2023-05-03 10:03:13 +0200

    Modify register inputs

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,9 +1,9 @@
 import { SyntheticEvent, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useUsers } from "../../hooks/useUsers";
-import { User } from "../../models/user";
+import { UserStructure } from "../../models/user";
 import { UsersRepo } from "../../services/users/users.repo";
-
+import "./register.css";
 export default function Register() {
   const repo = useMemo(() => new UsersRepo(), []);
   const { userRegister } = useUsers(repo);
@@ -13,9 +13,11 @@ export default function Register() {
     const formData = event.currentTarget as HTMLFormElement;
     const inputs = formData.querySelectorAll("input");
 
-    const newUser: Partial<User> = {
+    const newUser: Partial<UserStructure> = {
       email: inputs[0].value,
       password: inputs[1].value,
+      firstName: inputs[2].value,
+      lastName: inputs[3].value,
     };
 
     userRegister(newUser);
@@ -23,8 +25,9 @@ export default function Register() {
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form className="register__form" onSubmit={handleSubmit}>
         <input
+          className="register__forminput"
           type="email"
           name="email"
           id="email"
@@ -32,14 +35,33 @@ export default function Register() {
           placeholder="Email"
         />
         <input
+          className="register__forminput"
           type="password"
           name="password"
           id="password"
           required
           placeholder="Password"
         />
-        <button type="submit">Register</button>
-        <div>
+        <input
+          className="register__forminput"
+          type="firstName"
+          name="firstName"
+          id="firstName"
+          required
+          placeholder="First name"
+        />
+        <input
+          className="register__forminput"
+          type="lastName"
+          name="lastName"
+          id="lastName"
+          required
+          placeholder="Last name"
+        />
+        <button className="button__register" type="submit">
+          Register
+        </button>
+        <div className="register__buttonlogin">
           <Link to="/login">Login</Link>
         </div>
       </form>
