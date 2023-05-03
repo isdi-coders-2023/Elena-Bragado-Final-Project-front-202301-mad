921f2b03be67b2c539ec57ea459ac882a3e9cfcc
Elena <ebromeo88@gmail.com>
2023-05-03 10:02:21 +0200

    Modify login inputs

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,31 +1,41 @@
 import { SyntheticEvent, useMemo } from "react";
 import { useUsers } from "../../hooks/useUsers";
 import { UsersRepo } from "../../services/users/users.repo";
-import { User } from "../../models/user";
-
-import { Link } from "react-router-dom";
+import { UserStructure } from "../../models/user";
+import { Link, useNavigate } from "react-router-dom";
+import { useProfessionals } from "../../hooks/useProfessionals";
+import { ProfessionalsRepo } from "../../services/professionals/professional.repo";
+import "./login.css";
 
 export default function Login() {
-  const repo = useMemo(() => new UsersRepo(), []);
-  const { userLogin } = useUsers(repo);
+  const repoUser = useMemo(() => new UsersRepo(), []);
+  const repoProfessional = useMemo(() => new ProfessionalsRepo(), []);
+  const { userLogin } = useUsers(repoUser);
+  const { professionals } = useProfessionals(repoProfessional);
 
-  const handleSubmit = (event: SyntheticEvent) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = event.currentTarget as HTMLFormElement;
     const inputs = formData.querySelectorAll("input");
 
-    const newUser: Partial<User> = {
+    const userLogged: Partial<UserStructure> = {
       email: inputs[0].value,
       password: inputs[1].value,
     };
 
-    userLogin(newUser);
+    userLogin(userLogged);
+    // professionals();
     formData.reset();
+
+    navigate("/professionals/add");
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form className="login__form" onSubmit={handleSubmit}>
         <input
+          className="login__forminput"
           type="email"
           name="email"
           id="email"
@@ -33,14 +43,17 @@ export default function Login() {
           placeholder="Email"
         />
         <input
+          className="login__forminput"
           type="password"
           name="password"
           id="password"
           required
           placeholder="Password"
         />
-        <button type="submit">Login</button>
-        <div>
+        <button className="button__login" type="submit">
+          Login
+        </button>
+        <div className="register__buttonregister">
           <Link to="/register">Register</Link>
         </div>
       </form>
